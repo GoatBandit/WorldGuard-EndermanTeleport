71d042063deda1025699c4da3752d135f7168a4f
Encode42 <me@encode42.dev>
2020-10-08 23:36:21 -0400

    Some cleaning before release

--- src/main/java/dev/encode42/wgetp/WGETp.java
+++ src/main/java/dev/encode42/wgetp/WGETp.java
@@ -2,12 +2,10 @@ package dev.encode42.wgetp;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import org.bukkit.entity.EntityType;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.*;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTeleportEvent;
@@ -22,16 +20,12 @@ public final class WGETp extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
-	// Register the flag
+	// Inject the flag
 	@Override
 	public void onLoad() {
 		try {
-			FlagRegistry reg = WorldGuard.getInstance().getFlagRegistry();
-
-			StateFlag endermanTeleport = new StateFlag("enderman-teleport", true);
-			reg.register(endermanTeleport);
-
-			ENDERMAN_TELEPORT = endermanTeleport;
+			ENDERMAN_TELEPORT = new StateFlag("enderman-teleport", true);
+			WorldGuard.getInstance().getFlagRegistry().register(ENDERMAN_TELEPORT);
 		} catch (Exception e) {
 			getLogger().warning("The \"enderman-teleport\" flag is taken by another plugin!");
 			setEnabled(false);
@@ -46,9 +40,7 @@ public final class WGETp extends JavaPlugin implements Listener {
 		// Exit if region doesn't deny
 		com.sk89q.worldedit.util.Location location = BukkitAdapter.adapt(event.getEntity().getLocation());
 		RegionQuery query = WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery();
-		ApplicableRegionSet set = query.getApplicableRegions(location);
-
-		if (set.testState(null, ENDERMAN_TELEPORT)) return;
+		if (query.getApplicableRegions(location).testState(null, ENDERMAN_TELEPORT)) return;
 
 		// Cancel the teleportation
 		event.setCancelled(true);
